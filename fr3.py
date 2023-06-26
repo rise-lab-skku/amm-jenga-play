5ec149db231dfed7a05838769aa721d8149174c9
Taeha Shin <mirinae3145@g.skku.edu>
2023-06-26 13:43:22 +0900

    qdlim refer

--- fr3.py
+++ fr3.py
@@ -1,9 +1,10 @@
-#modified 06-20
+#modified 06-26
 
 import numpy as np
 from roboticstoolbox.robot.Robot import Robot
 from spatialmath import SE3
 import os
+import re
 
 class fr3(Robot):
     def __init__(self):
@@ -14,20 +15,28 @@ class fr3(Robot):
             xacro_tld="franka_description"
         )
         #added top level directory
+
+        qdlim=[]
+        l=[i.start() for i in re.finditer(" velocity",urdf_string)]
+        for i in range(7):
+            idx=urdf_string.find('"',l[i]+11)
+            qdlim.append(urdf_string[l[i]+11:idx])
+        #read qdlim from urdf, not by hardcoding
+
+        
+        
         super().__init__(
             links,name=name,gripper_links=links[17],urdf_string=urdf_string,urdf_filepath=urdf_filepath
         )
-        
         #calling Robot class, not sure what it exactly do
 
         self.grippers[0].tool=SE3(0,0,0.1034)
-        #seems to be edited if gripper changes
+        #end-effector's position
 
-        self.qdlim=np.array([2,1,1.5,1.25,3,1.5,3])
+        self.qdlim=np.array(qdlim)
         self.qr = np.array([0, -0.3, 0, -2.2, 0, 2.0, np.pi / 4])
         self.qz = np.zeros(7)
 
-        self.addconfiguration("qdlim(means velocity limit)",self.qdlim)
         self.addconfiguration("qr(ready position)", self.qr)
         self.addconfiguration("qz(zero joint angle)", self.qz)
 
@@ -43,3 +52,4 @@ if __name__=="__main__": #this is for debugging, add value desired to see, run o
     '''
     
     print(r.grippers[0].tool)
+    print(r.qdlim)
