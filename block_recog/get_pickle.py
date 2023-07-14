548abf7372bb11c67d0aa64aa66f7e242cf1330e
Hyun-soo-Shin <tls991105@naver.com>
2023-07-14 20:00:36 +0900

    temp_commit

---
+++ block_recog/get_pickle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import numpy as np
+import cv2
+import glob
+import open3d as o3d
+from matplotlib import pyplot as plt
+from func import *
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import pickle
+
+bridge = CvBridge()
+
+def image_callback1(msg):
+    # Convert ROS images to OpenCV format
+    img_color = bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+    img_depth = bridge.imgmsg_to_cv2(msg, desired_encoding='16UC1')
+    
+    ## Save pickle
+    with open('rgb.p', 'wb') as rgb:
+        pickle.dump(img_color, rgb)
+        
+def image_callback2(msg):
+    # Convert ROS images to OpenCV format
+    img_depth = bridge.imgmsg_to_cv2(msg, desired_encoding='16UC1')
+    
+    ## Save pickle
+    with open('rgb.p', 'wb') as rgb:
+        pickle.dump(img_depth, rgb)
+
+
+def main():
+    rospy.init_node('image_processing_node')
+    rospy.Subscriber('/rgb/image_raw', Image, image_callback)
+    rospy.Subscriber('/depth_to_rgb/image_raw', Image, image_callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
